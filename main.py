9b64c5ccdce51bb7f37335c2730549e700adc399
Jazylucky <81566910+Jazylucky@users.noreply.github.com>
2022-05-23 09:05:00 +0200

    Aktualisiere pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
+entf = 0
+
+def send():
+    pass   
+
+
 def on_forever():
-    pass
+    basic.pause(100)
+    entf = callibot.entfernung(KEinheit.CM)
+
+
+
+    if entf <= 7:
+        callibot.motor_stop(KMotor.BEIDE, KStop.BREMSEN)
+
+    if callibot.read_line_sensor(KSensor.LINKS, KSensorStatus.DUNKEL) or callibot.read_line_sensor(KSensor.RECHTS, KSensorStatus.DUNKEL):
+        callibot.motor_stop(KMotor.BEIDE, KStop.BREMSEN)
+        
+    
+        
+
+    
+
+
+
+    
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+let entf = 0
+function send() {
+    
+}
+
 basic.forever(function on_forever() {
+    basic.pause(100)
+    let entf = callibot.entfernung(KEinheit.cm)
+    if (entf <= 7) {
+        callibot.motorStop(KMotor.beide, KStop.Bremsen)
+    }
+    
+    if (callibot.readLineSensor(KSensor.links, KSensorStatus.dunkel) || callibot.readLineSensor(KSensor.rechts, KSensorStatus.dunkel)) {
+        callibot.motorStop(KMotor.beide, KStop.Bremsen)
+    }
     
 })
